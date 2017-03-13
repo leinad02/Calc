e1fcdf051c1ed3cb3790c84e4fa176e9fa91eb67
Daniel <danielri@edu.aau.at>
2017-03-13 09:08:29 +0100

    MainActivity.java Sourcecode für Rechenfunktion activity_main.xml Grundlegendes Layout (wird noch überarbeitet)

--- app/src/main/java/com/example/danie/calc/MainActivity.java
+++ app/src/main/java/com/example/danie/calc/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.danie.calc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText eingabe1;
+    EditText eingabe2;
+    Button dividiere;
+    TextView ergebnis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Eingabe der beiden Werte, Divisionsbutton und Ergebnis
+        eingabe1 = (EditText) findViewById(R.id.eTeingabe1);
+        eingabe2 = (EditText) findViewById(R.id.eTeingabe2);
+        dividiere = (Button) findViewById(R.id.btndiv);
+        ergebnis = (TextView) findViewById(R.id.twQuotient);
+
+        dividiere.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v){
+                //Eingabe wird zuerst zu String und dann zu Float geparst
+                float wert1 = Float.parseFloat(eingabe1.getText().toString());
+                float wert2 = Float.parseFloat(eingabe2.getText().toString());
+                float resultat = wert1/wert2;
+                ergebnis.setText(Float.toString(resultat));
+            }
+        });
+
     }
 }
